f7bb74ddd8b94d7b7d72b8094f3f27c3f0a8ce80
chaqui <josue.chaqui@gmail.com>
2018-09-20 22:40:02 -0600

    formulario para agregar un nuevo medio listo

--- SGREB/Controlador/MedioSolicitud.cs
+++ SGREB/Controlador/MedioSolicitud.cs
@@ -1,17 +1,82 @@
-
-using System;
+using SGREB.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+namespace SGREB.Controlador
+{
+    /// <summary>
+    /// Clase para controlar las actividades en la tabla TV_MedioSolicitud
+    /// </summary>
+    public class MedioSolicitud  {
+        /// <summary>
+        /// Constructor para crear objetos de funcionalidades
+        /// </summary>
+        public MedioSolicitud() {
 
-public class MedioSolicitud : CRUD {
+        }
+        /// <summary>
+        /// crear tvMedioSolucitud en la base de datos 
+        /// </summary>
+        /// <param name="tvMedioSolicitud"></param>
+        /// <returns>id del Medio de Solicitud creado</returns>
+        public int crear(TV_MedioSolicitud tvMedioSolicitud)
+        {
+            var bitacora = new bitacoraBomberoaContext();
+            bitacora.TV_MedioSolicitud.Add(tvMedioSolicitud);
+            bitacora.SaveChanges();
+            return tvMedioSolicitud.idSolicitud;
+        }
 
-    public MedioSolicitud() {
+        /// <summary>
+        /// modificar tvMedioSolicitud
+        /// </summary>
+        /// <param name="tvMedioSolicitud"></param>
+
+        public void Modificar(TV_MedioSolicitud tvMedioSolicitud)
+        {
+            using (var bitacora = new bitacoraBomberoaContext())
+            {
+                var tvMedioSolicitudM = bitacora.TV_MedioSolicitud.Find(tvMedioSolicitud.idSolicitud);
+                tvMedioSolicitudM.medio = tvMedioSolicitud.medio;
+                bitacora.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// obrtiene todos los Medios
+        /// </summary>
+        /// <returns>Lista de tvMedioSolicitudM
+        /// que se encuentran en la Base de Datos</returns>
+        public List<TV_MedioSolicitud> obtenerTodos()
+        {
+            var bitacora = new bitacoraBomberoaContext();
+            var tvMedioSolicitud = bitacora.TV_MedioSolicitud;
+            return tvMedioSolicitud.ToList();
+        }
+
+        /// <summary>
+        /// obtien el medio de Solicitud por su Id
+        /// </summary>
+        /// <param name="id">id del Medio de solicitud</param>
+        /// <returns>Objeto con la información de medio de solicitud</returns>
+        public TV_MedioSolicitud obtener(int id)
+        {
+            var bitacora = new bitacoraBomberoaContext();
+            var tV_MedioSolicitud = bitacora.TV_MedioSolicitud.Where(s => s.idSolicitud == id).Single();
+            return tV_MedioSolicitud;
+        }
+
+        /// <summary>
+        /// obtiene el medio de solicitud por su nombre
+        /// </summary>
+        /// <param name="nombre">nombre del medio a buscar</param>
+        /// <returns>lista de medios de solicitud que contienen el nombre</returns>
+        public List<TV_MedioSolicitud> obtener(string nombre)
+        {
+            var bitacora = new bitacoraBomberoaContext();
+            var tV_MedioSolicitud = bitacora.TV_MedioSolicitud.Where(s => s.medio == nombre);
+            return tV_MedioSolicitud.ToList();
+        }
+
+    
     }
-
-    private void idMedio;
-
-    private void nombre;
-
-
 }
---
+++ SGREB/Formularios/MedioForm.xaml.cs
@@ -0,0 +1,50 @@
+﻿using SGREB.Controlador;
+using SGREB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SGREB.Formularios
+{
+    /// <summary>
+    /// Lógica de interacción para MedioForm.xaml
+    /// </summary>
+    public partial class MedioForm : Window
+    {
+        public MedioForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// funcion para guardar el medio
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = txNombre.Text;
+            if(nombre == "")
+            {
+                MessageBox.Show("no a ingresado ningun nombre", "error");
+                return;
+            }
+            TV_MedioSolicitud tvMedio = new TV_MedioSolicitud();
+            tvMedio.medio = nombre;
+            MedioSolicitud medioSolicitud = new MedioSolicitud();
+            medioSolicitud.crear(tvMedio);
+            MessageBox.Show("Medio creado");
+            this.Close();
+        }
+    }
+}
